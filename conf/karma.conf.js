55a09236e100cbeb59b980d6bca0740c5d97d3fb
marlou <pro@marc-loupias.fr>
2016-12-08 21:48:29 +0100

    coverage - config

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -2,45 +2,51 @@ const conf = require('./gulp.conf');
 const listFiles = require('./karma-files.conf');
 
 module.exports = function (config) {
-  const configuration = {
-    basePath: '../',
-    singleRun: true,
-    autoWatch: false,
-    logLevel: 'INFO',
-    junitReporter: {
-      outputDir: 'test-reports'
-    },
-    browsers: [
-      'PhantomJS'
-    ],
-    frameworks: [
-      'phantomjs-shim',
-      'jasmine',
-      'angular-filesort'
-    ],
-    files: listFiles(),
-    preprocessors: {
-        '**/*.html': ['ng-html2js']
-    },
-    ngHtml2JsPreprocessor: {
-      stripPrefix: 'src/',
-      moduleName: 'app'
-    },
-    angularFilesort: {
-      whitelist: [
-        conf.path.tmp('**/!(*.html|*.spec|*.mock).js')
-      ]
-    },
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-junit-reporter'),
-      require('karma-coverage'),
-      require('karma-phantomjs-launcher'),
-      require('karma-phantomjs-shim'),
-      require('karma-ng-html2js-preprocessor'),
-      require('karma-angular-filesort')
-    ]
-  };
+    const configuration = {
+        basePath: '../',
+        singleRun: true,
+        autoWatch: false,
+        logLevel: 'INFO',
+        junitReporter: {
+            outputDir: 'test-reports'
+        },
+        browsers: [
+            'PhantomJS'
+        ],
+        frameworks: [
+            'phantomjs-shim',
+            'jasmine',
+            'angular-filesort'
+        ],
+        files: listFiles(),
+        reporters: ['progress', 'coverage'],
+        preprocessors: {
+            '**/*.html': ['ng-html2js'],
+            '.tmp/**/*.js': ['coverage']
+        },
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'src/',
+            moduleName: 'app'
+        },
+        angularFilesort: {
+            whitelist: [
+                conf.path.tmp('**/!(*.html|*.spec|*.mock).js')
+            ]
+        },
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        },
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-junit-reporter'),
+            require('karma-coverage'),
+            require('karma-phantomjs-launcher'),
+            require('karma-phantomjs-shim'),
+            require('karma-ng-html2js-preprocessor'),
+            require('karma-angular-filesort')
+        ]
+    };
 
-  config.set(configuration);
+    config.set(configuration);
 };
