13969d153d12e1a6b8449f99e5b4ce69074352dc
marlou <pro@marc-loupias.fr>
2016-12-11 13:47:18 +0100

    UT - cv-loader.service.spec.js refacto

--- src/app/common/loader/cv-loader.service.spec.js
+++ src/app/common/loader/cv-loader.service.spec.js
@@ -3,65 +3,41 @@
 
     describe('app.common.loader.loaderService', function () {
         describe('when I call loaderService.isLoadingDone()', function () {
-            var loaderService;
-
             beforeEach(function () {
                 angular.mock.module('app.common.loader');
-                angular.mock.inject(injectLoaderService);
-
-                function injectLoaderService(_loaderService_) {
-                    loaderService = _loaderService_;
-                    expect(typeof loaderService).toEqual('object');
-                }
             });
 
-            it('should return true if loaderService.data.done equal true', function () {
+            it('should return true if loaderService.data.done equal true', angular.mock.inject(function (loaderService) {
                 loaderService.data.done = true;
                 expect(loaderService.isLoadingDone()).toBeTruthy();
-            });
+            }));
 
-            it('should return false if loaderService.data.done equal false', function () {
+            it('should return false if loaderService.data.done equal false', angular.mock.inject(function (loaderService) {
                 loaderService.data.done = false;
                 expect(loaderService.isLoadingDone()).toBeFalsy();
-            });
+            }));
         });
 
         describe('when I call loaderService.start()', function () {
-            var loaderService;
-
             beforeEach(function () {
                 angular.mock.module('app.common.loader');
-                angular.mock.inject(injectLoaderService);
-
-                function injectLoaderService(_loaderService_) {
-                    loaderService = _loaderService_;
-                    expect(typeof loaderService).toEqual('object');
-                }
             });
 
-            it('should set loaderService.data.done to false', function () {
+            it('should set loaderService.data.done to false', angular.mock.inject(function (loaderService) {
                 loaderService.start();
                 expect(loaderService.data.done).toBeFalsy();
-            });
+            }));
         });
 
         describe('when I call loaderService.done()', function () {
-            var loaderService;
-
             beforeEach(function () {
                 angular.mock.module('app.common.loader');
-                angular.mock.inject(injectLoaderService);
-
-                function injectLoaderService(_loaderService_) {
-                    loaderService = _loaderService_;
-                    expect(typeof loaderService).toEqual('object');
-                }
             });
 
-            it('should set loaderService.data.done to true', function () {
+            it('should set loaderService.data.done to true', angular.mock.inject(function (loaderService) {
                 loaderService.done();
                 expect(loaderService.data.done).toBeTruthy();
-            });
+            }));
         });
     });
 
